17975eae7eb08f1f400f01f64024b0ce1089fde9
Avinash Kodakandla <avinashk@vaycayhero.com>
2015-05-10 21:01:08 +0530

    updated the retention and target

--- src/main/java/com/jvalidation/annotations/Empty.java
+++ src/main/java/com/jvalidation/annotations/Empty.java
@@ -1,5 +1,15 @@
 package com.jvalidation.annotations;
 
-public @interface Empty {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target({ElementType.FIELD,ElementType.PARAMETER,ElementType.LOCAL_VARIABLE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Empty {
+	
+	public String errorMessage();
+	public String logMessage();
+	
 }
